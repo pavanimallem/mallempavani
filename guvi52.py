6bbeedaa14aecf17f51f654f17e12de4749ecdf5
pavanimallem <41769826+pavanimallem@users.noreply.github.com>
2018-08-28 20:45:55 +0530

    Update guvi52.py

--- guvi52.py
+++ guvi52.py
@@ -1,3 +1,3 @@
 n=int(input(" "))
-words=['one','two','three','four','five','six','seven','eight','nine','ten']
+words=['One','Two','Three','Four','Five','Six','Seven',Eight','Nine','Ten']
 print(words[(n)-1])
